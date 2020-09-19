d78eee8bea0244e3cc2b324e676fb7ff7dbdc2f0
Jason Antwi-Appah <hey@jasonaa.me>
2020-09-19 00:27:07 -0500

    Testing out routing stuff

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useRouteMatch,
+  Link,
+} from "react-router-dom";
 
 function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/test">
+          <Content />
+        </Route>
+        <Route path="/">
+          <Content />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+function Content() {
+  let match = useRouteMatch();
+  console.log(match);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Link to={match.path === "/test" ? "/" : "/test"}>
+            Edit <code>src/App.js</code> and save to reload.
+          </Link>
+        </p>
+        <p>
+          {match.url} | {match.path} | {match.name}
         </p>
         <a
           className="App-link"
@@ -22,5 +52,33 @@ function App() {
     </div>
   );
 }
+
+// function Content2() {
+//   let match = useRouteMatch();
+//   console.log(match);
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           <Link to="/">
+//             Edit <code>src/ANOOOOOOOOOpp.js</code> and save to reload.
+//           </Link>
+//         </p>
+//         <p>
+//           {match.url} yeet {match.path} yeet {match.name}
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
