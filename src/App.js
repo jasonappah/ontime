5887c3f293793474611a9505fab1df7a6c716e4f
Jason Antwi-Appah <hey@jasonaa.me>
2020-09-19 21:52:03 -0500

    Got Firebase auth working!!!!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-// yeet
-
-import React from "react";
+import React, {useState} from "react";
 
 import {
   GeistProvider,
@@ -13,9 +11,38 @@ import {
   Link,
 } from "@geist-ui/react";
 
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "firebase";
+
 import { Github, ExternalLink, Twitter } from "@geist-ui/react-icons";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBVDtBhFaV0Lhwi6Aierm-qUOpBrhsXOgU",
+  authDomain: "ontime-jasonaa.firebaseapp.com",
+  databaseURL: "https://ontime-jasonaa.firebaseio.com",
+  projectId: "ontime-jasonaa",
+  storageBucket: "ontime-jasonaa.appspot.com",
+  messagingSenderId: "180943670738",
+  appId: "1:180943670738:web:ae35d21d9cde1ba91e8c0b",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: "popup",
+  callbacks: {
+    // Avoid redirects after sign-in.
+    signInSuccessWithAuthResult: () => false
+  },
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
+  ],
+};
 
 function App() {
   return (
@@ -23,11 +50,11 @@ function App() {
       <CssBaseline />
       <Router>
         <Switch>
-          <Route path="/">
-            <Landing />
-          </Route>
           <Route path="/signin">
             <Login />
+          </Route>
+          <Route path="/">
+            <Landing />
           </Route>
         </Switch>
       </Router>
@@ -36,13 +63,39 @@ function App() {
 }
 
 function Login() {
+  const [isSignedIn, authChange] = useState(false)
+
+  const unregisterAuthObserver = firebase.auth().onAuthStateChanged(
+    (user) => authChange(!!user))
+  
+  if (!isSignedIn) {
   return (
     <Page dotBackdrop size="large">
       <Page.Content>
         <Text h1>Sign in / register here.</Text>
+        <Card shadow>
+          <Text h3>Select your preferred login method.</Text>
+          <StyledFirebaseAuth
+            uiConfig={uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        </Card>
       </Page.Content>
     </Page>
   );
+  } else {
+    return (
+      <Page dotBackdrop size="large">
+      <Page.Content>
+        <Text h1>You're in.</Text>
+        <Card shadow>
+          <Text h3>{firebase.auth().currentUser.displayName}, {firebase.auth().currentUser.email}, {firebase.auth().currentUser.uid}, {firebase.auth().currentUser.providerId}</Text>
+            <Link onClick={() => {firebase.auth().signOut(); unregisterAuthObserver()}}>Sign-out</Link>
+        </Card>
+      </Page.Content>
+    </Page>
+    )
+}
 }
 
 function Landing() {
